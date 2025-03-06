d8ffd0e7a9434d46850bf9073efbcc56ad5a396d
Ethan Mylett <ethan.mylett@yahoo.com>
2025-03-06 16:49:27 -0500

    added styling for multiple tide locations

--- src/TideLocationList/TideLocationList.jsx
+++ src/TideLocationList/TideLocationList.jsx
@@ -1,10 +1,12 @@
 import { TideDependents } from "../TideDependents/TideDependents";
+import "./TideLocationList.css";
+
 export const TideLocationList = ({ stationIds }) => {
   return (
-    <>
+    <div className="tide-location-list">
       {stationIds.map((stationId, index) => {
         return <TideDependents id={stationId} key={index} />;
       })}
-    </>
+    </div>
   );
 };
