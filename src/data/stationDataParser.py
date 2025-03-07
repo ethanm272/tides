4356b33c627b63604496f68ba64ad96a1253e165
Ethan Mylett <ethan.mylett@yahoo.com>
2025-03-07 15:59:19 -0500

    florida stations data

---
+++ src/data/stationDataParser.py
@@ -0,0 +1,10 @@
+import json
+
+stations = []
+with open('src/data/floridaStationData.txt', 'r') as file:
+    for line in file:
+        lineArr = line.split()
+        stations.append({'id': lineArr[0], 'name': lineArr[1]})
+
+with open('data.json', 'w', encoding='utf-8') as jsonFile:
+    json.dump({'stations': stations}, jsonFile, ensure_ascii=False, indent=2)
